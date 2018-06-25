a460d470e382b423fd06cda8595deee288a8f37f
jomz <hi@monkeypatch.be>
2018-06-25 15:47:54 +0200

    Bump version for gem release

--- lib/radiant-find_replace-extension.rb
+++ lib/radiant-find_replace-extension.rb
@@ -1,5 +1,5 @@
 module RadiantFindReplaceExtension
-  VERSION     = "1.0.5"
+  VERSION     = "1.0.6"
   SUMMARY     = "Find Replace for Radiant CMS"
   DESCRIPTION = "Adds backend search and replace functionality to Radiant CMS!"
   URL         = "https://github.com/jomz/radiant-find_replace-extension"
